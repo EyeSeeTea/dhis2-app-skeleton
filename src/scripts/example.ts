e778d2d741f8076ed05b37bbc1b566e03ea23fba
Arnau Sanchez <pyarnau@gmail.com>
2023-11-22 23:54:36 +0100

    Fix example.ts for node 18

--- src/scripts/example.ts
+++ src/scripts/example.ts
@@ -2,6 +2,9 @@ import { command, run, string, option } from "cmd-ts";
 import path from "path";
 import { D2Api } from "../types/d2-api";
 
+// Usage:
+// npx ts-node src/scripts/example.ts -u "https://dev.eyeseetea.com/play" -a admin:district
+
 function main() {
     const cmd = command({
         name: path.basename(__filename),
@@ -9,13 +12,24 @@ function main() {
         args: {
             url: option({
                 type: string,
-                long: "dhis2-url",
+                long: "url",
                 short: "u",
-                description: "DHIS2 base URL. Example: http://USERNAME:PASSWORD@localhost:8080",
+                description: "DHIS2 base URL. Example: http://localhost:8080",
+            }),
+            auth: option({
+                type: string,
+                long: "auth",
+                short: "a",
+                description: "DHIS2 auth: USERNAME:PASSWORD",
             }),
         },
         handler: async args => {
-            const api = new D2Api({ baseUrl: args.url });
+            const [username = "", password = ""] = args.auth.split(":");
+            const api = new D2Api({
+                backend: "xhr",
+                baseUrl: args.url,
+                auth: { username: username, password: password },
+            });
             const info = await api.system.info.getData();
             console.debug(info);
         },
