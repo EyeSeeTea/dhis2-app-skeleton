d16dffc971441a0a6fba43b11267ddd5713daa1b
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2024-06-26 12:03:31 +0100

    chore: set default app namespace for translations

--- src/webapp/contexts/app-context.ts
+++ src/webapp/contexts/app-context.ts
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import { CompositionRoot } from "$/CompositionRoot";
 import { User } from "$/domain/entities/User";
+// @ts-ignore
+import i18n from "$/locales";
 
 export interface AppContextState {
     currentUser: User;
@@ -11,6 +13,7 @@ export const AppContext = React.createContext<AppContextState | null>(null);
 
 export function useAppContext() {
     const context = useContext(AppContext);
+    i18n.setDefaultNamespace("dhis2-skeleton-app");
     if (context) {
         return context;
     } else {
