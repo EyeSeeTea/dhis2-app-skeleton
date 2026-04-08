ee13936bae43c391a25c737e3457fc4d298ec4fe
Arnau Sanchez <pyarnau@gmail.com>
2026-04-08 12:28:59 +0200

    refactor: use mui styles instead of removed styled-components

--- src/webapp/pages/app/header-bar/HeaderBar.tsx
+++ src/webapp/pages/app/header-bar/HeaderBar.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { styled } from "@material-ui/styles";
 import { HeaderBar as D2HeaderBar } from "@dhis2/ui";
 
 type HeaderBarProps = {
@@ -6,15 +6,15 @@ type HeaderBarProps = {
 };
 
 // avoid rendering header for versions > 2.41
-//https://developers.dhis2.org/docs/references/global-shell/#header-bars
+// https://developers.dhis2.org/docs/references/global-shell/#header-bars
 export const HeaderBar: React.FC<HeaderBarProps> = props => {
     const { appName } = props;
     const shouldRenderHeaderBar = window.self === window.top;
     return shouldRenderHeaderBar && <StyledHeaderBar appName={appName} />;
 };
 
-const StyledHeaderBar = styled(D2HeaderBar)`
-    div:first-of-type {
-        box-sizing: border-box;
-    }
-`;
+const StyledHeaderBar = styled(D2HeaderBar)({
+    "& div:first-of-type": {
+        boxSizing: "border-box",
+    },
+});
