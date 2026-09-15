2ec4f48266143cc0aaff8d5204642d3cb106842f
Matias Arriola <matias@eyeseetea.com>
2026-09-15 08:58:04 -0300

    chore: revert old GenericStruct rename

--- src/domain/entities/generic/Struct.ts
+++ src/domain/entities/generic/Struct.ts
@@ -40,6 +40,6 @@ export function Struct<Attrs>() {
     };
 }
 
-const _GenericStruct = Struct<unknown>();
+export const GenericStruct = Struct<unknown>();
 
-export type GenericStructInstance = InstanceType<typeof _GenericStruct>;
+export type GenericStructInstance = InstanceType<typeof GenericStruct>;
